6032726681f6d1a319f694a923a62c26ca1b9be9
Andrewlv <2289380832@qq.com>
2024-11-26 14:24:54 +0800

    初始化项目模版

---
+++ src/main/java/com/andrew/mianshidog/constant/FileConstant.java
@@ -0,0 +1,15 @@
+package com.andrew.mianshidog.constant;
+
+/**
+ * 文件常量
+ *
+
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://andrew.icu";
+}
